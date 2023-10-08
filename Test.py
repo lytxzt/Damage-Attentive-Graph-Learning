5cf8e93c50935e0d3ca1b495a5d3342c28f63deb
lytxzt <lytxzt@gmail.com>
2023-10-08 15:53:09 +0800

    123

--- Test.py
+++ Test.py
@@ -8,9 +8,12 @@ A = Utils.make_A_matrix(positions, len(positions), 100)
 D = Utils.make_D_matrix(A, len(positions))
 L = D - A
 connected_flag, num_of_clusters = Utils.check_number_of_clusters(L, len(positions))
-print(num_of_clusters)
+# print(num_of_clusters)
 
-e_vals, e_vecs = np.linalg.eig(A)
-# print(e_vals, e_vecs)
-print(max(e_vals), np.argmax(e_vals))
-print(e_vecs[:, np.argmax(e_vals)], np.var(e_vecs[np.argmax(e_vals),:]))
+# e_vals, e_vecs = np.linalg.eig(A)
+# # print(e_vals, e_vecs)
+# print(max(e_vals), np.argmax(e_vals))
+# print(e_vecs[:, np.argmax(e_vals)], np.var(e_vecs[np.argmax(e_vals),:]))
+
+degree = np.sum(A, axis=0)
+print(degree, np.var(degree))
